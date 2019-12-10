ea124080487ca8c181ed4050ce02d66eacc1cdcf
Isaac Avilez <isaac1180@aol.com>
2019-12-10 16:48:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog 
-  def name(name)
+  def name
     @name
   end
   
   def name=(name)
     @name = name 
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
   end
 end
